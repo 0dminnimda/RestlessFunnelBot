06d539c0088bcf568ff3b9a7be92d83680aec50e
0dminnimda <0dminnimda@gmail.com>
2022-10-22 17:50:30 +0300

    models.py: remove useless stuff

--- RestlessFunnelBot/models.py
+++ RestlessFunnelBot/models.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from enum import Enum
 from typing import List, Optional
 
-from sqlmodel import Field, SQLModel, ARRAY, Integer, Relationship, JSON
 from sqlalchemy.sql.schema import Column
+from sqlmodel import JSON, Field, SQLModel
 
 
 class Platform(Enum):
@@ -21,25 +21,9 @@ class Message(SQLModel, table=True):
     platform: Platform
 
 
-# class ChatUserLink(SQLModel, table=True):
-#     chat_id: int = Field(foreign_key="chat.id", primary_key=True)
-#     user_id: int = Field(foreign_key="user.id", primary_key=True)
-
-
-# class Chat(SQLModel, table=True):
-#     id: int = Field(primary_key=True)
-#     users: List["User"] = Relationship(
-#         back_populates="accessible_chats", link_model=ChatUserLink
-#     )
-#     platform: Platform
-
-
 class User(SQLModel, table=True):
     id: int = Field(primary_key=True)
     accessible_chats: List[int] = Field(default_factory=list, sa_column=Column(JSON))
-    # accessible_chats: List[Chat] = Relationship(
-    #     back_populates="users", link_model=ChatUserLink
-    # )
     platform: Platform
 
     def add_chat(self, id: int) -> None:
