a6e181d188ad7068abf41d66e1f0ac20202239fe
0dminnimda <0dminnimda@gmail.com>
2022-10-22 04:23:02 +0300

    manual.py: update for it to work with db

--- manual.py
+++ manual.py
@@ -1,5 +1,14 @@
 # Temporary manual test
 
+from asyncio import run as run_async
+
+from RestlessFunnelBot.db import db_tables
 from RestlessFunnelBot.telegram import run
 
-run()
+
+async def main():
+    async with db_tables():
+        await run()
+
+
+run_async(main())
