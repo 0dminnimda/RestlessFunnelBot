8e6ebb3297350ef59f6fcd39c2805fa9505cb0a2
0dminnimda <0dminnimda@gmail.com>
2022-11-01 02:11:11 +0300

    telegram_bot.py: use common and clean up

--- RestlessFunnelBot/telegram_bot.py
+++ RestlessFunnelBot/telegram_bot.py
@@ -10,6 +10,7 @@ from aiogram.types import Message as TargetMessage
 from aiogram.types import User as TargetUser
 
 from . import secrets
+from .common import handle_new_message
 from .database import make_db
 from .mappers import map_model, model_mapper
 from .models import TELEGRAM as PLATFORM
@@ -51,36 +52,20 @@ async def send_welcome(in_msg: TargetMessage):
     await in_msg.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
 
-@dp.message_handler(commands=["list"])
-async def list_command(in_msg: TargetMessage):
-    async with make_db(PLATFORM) as db:
-        messages = []
-        for msg in await db.read_messages():
-            messages.append("-" * 50 + f"\n{msg.id}: {msg.text}")
-
-        await in_msg.reply("List of all messages\n" + "\n\n".join(messages))
-
-
-@dp.message_handler(commands=["chats"])
-async def chats_command(in_msg: TargetMessage):
-    async with make_db(PLATFORM) as db:
-        user = await db.get_user(in_msg.from_user)
-        tasks = [bot.get_chat(chat_id) for chat_id in user.accessible_chats]
-        chats = [f"{i+1} {chat.title}" for i, chat in enumerate(await gather(*tasks))]
-        await in_msg.reply("List of accessible chats\n" + "\n".join(chats))
-
-
 @dp.message_handler()
 async def collect_messages(in_msg: TargetMessage):
+    # print("mes", in_msg.text, in_msg.chat.type)
+
+    if in_msg.chat.type != TargetChatType.GROUP:
+        return
+
     async with make_db(PLATFORM) as db:
-        print("mes", in_msg.text, in_msg.chat.type)
-        if in_msg.chat.type == TargetChatType.GROUP:
-            db.create_message(in_msg)
+        result = await handle_new_message(db, in_msg)
 
-            user = await db.get_user(in_msg.from_user)
-            user.add_chat(in_msg.chat.id)
+    if result:
+        await in_msg.reply(result)
 
-        await in_msg.answer(in_msg.text)
+    # await in_msg.answer(in_msg.text)
 
 
 async def run():
