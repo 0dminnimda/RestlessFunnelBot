43bdfebd9fa5daaee8b365b683a49a7a34526f61
0dminnimda <0dminnimda@gmail.com>
2022-10-31 23:20:55 +0300

    database.py: add general db functions, comment out specific

--- RestlessFunnelBot/database.py
+++ RestlessFunnelBot/database.py
@@ -1,15 +1,6 @@
 from contextlib import asynccontextmanager
 from pathlib import Path
-from typing import (
-    Any,
-    AsyncGenerator,
-    Callable,
-    Dict,
-    List,
-    Optional,
-    Type,
-    TypeVar,
-)
+from typing import Any, AsyncGenerator, Callable, Dict, List, Optional, Type, TypeVar
 
 from sqlalchemy.engine import Result, ScalarResult
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -17,7 +8,8 @@ from sqlalchemy.sql import Select
 from sqlmodel import SQLModel, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from .models import Message, Platform, User
+from .mappers import map_model
+from .models import Chat, Message, Platform, User
 
 DATABASE_URL = f"sqlite+aiosqlite:///{Path(__file__).parent}/sqlite.db"
 
@@ -52,19 +44,6 @@ async def db_tables() -> AsyncGenerator[None, None]:
 
 
 T = TypeVar("T", bound=Any)
-ModelMapper = Callable[[T], Dict[str, Any]]
-MapToModel = Dict[Type[T], ModelMapper]
-
-
-_model_mappers: MapToModel = {}
-
-
-def model_mapper(type_: Type[T]) -> Callable[[ModelMapper], ModelMapper]:
-    def inner(f: ModelMapper) -> ModelMapper:
-        _model_mappers[type_] = f
-        return f
-
-    return inner
 
 
 class DataBase(AsyncSession):
@@ -76,36 +55,103 @@ class DataBase(AsyncSession):
         result: Result = await self.execute(selection)
         return result.scalars()
 
-    def create_raw(self, model: Type[T], *args: Any, **kwargs: Any) -> T:
+    async def read_all(self, model: Type[T], **kwargs: Any) -> List[T]:
+        selection = select(model).filter_by(**kwargs)
+        return (await self.fetch(selection)).all()
+
+    async def read_one(self, model: Type[T], **kwargs: Any) -> T:
+        selection = select(model).filter_by(**kwargs)  # .first()
+        return (await self.fetch(selection)).one()
+
+    async def read_one_or_none(self, model: Type[T], **kwargs: Any) -> Optional[T]:
+        selection = select(model).filter_by(**kwargs)  # .first()
+        return (await self.fetch(selection)).one_or_none()
+
+    def create(self, model: Type[T], **kwargs: Any) -> T:
         kwargs["platform"] = self.platform
-        instance = model(*args, **kwargs)
+        instance = model(**kwargs)
         self.add(instance)
         return instance
 
-    def create(self, model: Type[T], obj: Any) -> T:
-        return self.create_raw(model, **_model_mappers[type(obj)](obj))
+    async def read_or_create(self, model: Type[T], **kwargs: Any) -> T:
+        kwargs["platform"] = self.platform
+        instance = await self.read_one_or_none(model, **kwargs)
+        if instance is not None:
+            return instance
+        return self.create(model, **kwargs)
+        # instance = model(**kwargs)
+        # session.add(instance)
+        # session.commit()
+        # return instance
+
+    #     if not isinstance(obj, dict):
+    #         model = map_model(obj)
+    #     user = await self.read_user(model["id"])
+    #     if user is not None:
+    #         return user
+    #     return self.create_raw(User, **model)
+
+    # def create_raw(self, *args: Any, **kwargs: Any) -> T:
+    #     kwargs["platform"] = self.platform
+    #     instance = model(*args, **kwargs)
+    #     self.add(instance)
+    #     return instance
+
+    # async def create(self, obj: Any, **extra: Any) -> Any:
+    #     extra["platform"] = self.platform
+    #     return await map_model(obj, extra, self.get_or_create, recursive=True)
+    #     # return map_model(obj, extra, self.add, recursive=True)
+
+    # def create_raw(self, model: Type[T], *args: Any, **kwargs: Any) -> T:
+    #     kwargs["platform"] = self.platform
+    #     instance = model(*args, **kwargs)
+    #     self.add(instance)
+    #     return instance
+
+    # def create(self, model: Type[T], obj: Any) -> T:
+    #     return self.create_raw(model, **map_model(obj, recursive=True))
 
     # Message
-    def create_message(self, obj: Any) -> Message:
-        return self.create(Message, obj)
+    # def create_message(self, obj: Any) -> Message:
+    #     result = self.create(obj)
+    #     assert isinstance(result, Message)
+    #     return result
 
-    async def read_messages(self) -> List[Message]:
-        selection = select(Message)
-        return (await self.fetch(selection)).all()
+    # async def read_messages(self) -> List[Message]:
+    #     selection = select(Message)
+    #     return (await self.fetch(selection)).all()
 
     # User
-    def create_user(self, obj: Any) -> User:
-        return self.create(User, obj)
+    # def create_user(self, obj: Any) -> User:
+    #     return self.create(User, obj)
 
-    async def read_user(self, id: int) -> Optional[User]:
-        selection = select(User).filter(User.id == id)
-        return (await self.fetch(selection)).one_or_none()
+    # async def read_user(self, id: int) -> Optional[User]:
+    #     selection = select(User).filter(User.id == id)
+    #     return (await self.fetch(selection)).one_or_none()
 
-    async def get_user(self, id: int) -> User:
-        user = await self.read_user(id)
-        if user is not None:
-            return user
-        return self.create_raw(User, id=id)
+    # async def get_user(self, obj: Any) -> User:
+    #     if not isinstance(obj, dict):
+    #         model = map_model(obj)
+    #     user = await self.read_user(model["id"])
+    #     if user is not None:
+    #         return user
+    #     return self.create_raw(User, **model)
+
+    # Chat
+    # def create_chat(self, obj: Any) -> Chat:
+    #     return self.create(Chat, obj)
+
+    # async def read_chat(self, id: int) -> Optional[Chat]:
+    #     selection = select(Chat).filter(Chat.id == id)
+    #     return (await self.fetch(selection)).one_or_none()
+
+    # async def get_chat(self, obj: Any) -> Chat:
+    #     if not isinstance(obj, dict):
+    #         model = map_model(obj)
+    #     chat = await self.read_chat(model["id"])
+    #     if chat is not None:
+    #         return chat
+    #     return self.create_raw(Chat, **model)
 
 
 def make_session(platform: Platform) -> DataBase:
