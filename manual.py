5992231f6b2e644770d18644016e7cde0684e87d
0dminnimda <0dminnimda@gmail.com>
2022-11-01 02:11:22 +0300

    manual.py: fix import

--- manual.py
+++ manual.py
@@ -2,7 +2,7 @@
 
 from asyncio import run as run_async
 
-from RestlessFunnelBot import telegram_bot
+from RestlessFunnelBot import discord_bot, telegram_bot
 from RestlessFunnelBot.database import db_tables
 
 
