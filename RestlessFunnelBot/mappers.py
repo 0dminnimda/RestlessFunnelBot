7f298fa045838755a77fde3edab90f31416c24a1
0dminnimda <0dminnimda@gmail.com>
2022-11-01 02:23:57 +0300

    mappers.py: remove extra as an argument

--- RestlessFunnelBot/mappers.py
+++ RestlessFunnelBot/mappers.py
@@ -27,30 +27,24 @@ def _find_mapper_key(type: Type[T]) -> Optional[Type[Any]]:
         assert False, "Not Implemented yet"
 
 
-def _sub_map_model(
-    obj: Any, extra: Dict[str, Any], recursive: bool
-) -> Optional[Dict[str, Any]]:
+def _sub_map_model(obj: Any, recursive: bool) -> Optional[Dict[str, Any]]:
     key = _find_mapper_key(type(obj))
     if key is None:
         return None
     model, mapper = _model_mappers[key]
 
-    fields = {**extra, **mapper(obj)}
+    fields = mapper(obj)
     if recursive:
         for name, value in fields.items():
-            sub_result = _sub_map_model(value, extra, recursive)
+            sub_result = _sub_map_model(value, recursive)
             if sub_result is not None:
                 fields[name] = sub_result
 
     return fields
 
 
-def map_model(
-    obj: Any,
-    extra: Dict[str, Any] = dict(),
-    recursive: bool = False,
-) -> Dict[str, Any]:
-    result = _sub_map_model(obj, extra, recursive)
+def map_model(obj: Any, recursive: bool = False) -> Dict[str, Any]:
+    result = _sub_map_model(obj, recursive)
     if result is None:
         raise KeyError(f"No mapping found for type {type(obj)}")
     return result
