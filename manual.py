01f8d0d1de1cb5d277ac2a8583a499e155edd62e
0dminnimda <0dminnimda@gmail.com>
2022-11-02 01:32:45 +0300

    manual.py: add vk_bot to main

--- manual.py
+++ manual.py
@@ -3,13 +3,14 @@
 from asyncio import gather
 from asyncio import run as run_async
 
-from RestlessFunnelBot import discord_bot, telegram_bot
+from RestlessFunnelBot import discord_bot, telegram_bot, vk_bot
 from RestlessFunnelBot.database import db_tables
 
 
 async def main():
     async with db_tables():
         await gather(
+            vk_bot.run(),
             discord_bot.run(),
             telegram_bot.run(),
         )
