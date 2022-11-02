83bc59e5e7047782c04e5d59277032ce09177b36
0dminnimda <0dminnimda@gmail.com>
2022-11-02 05:06:42 +0300

    Create RestlessFunnelBot\messenger.py

---
+++ RestlessFunnelBot/messenger.py
@@ -0,0 +1,35 @@
+from typing import Any, Awaitable, Callable, Dict, Type, TypeVar
+
+T = TypeVar("T", bound=Any)
+Func = Callable[[T, str], Awaitable[None]]
+MapToFunc = Dict[Type[T], Func]
+
+
+_reply_functions: MapToFunc = {}
+
+
+def reply_function(type: Type[Any]) -> Callable[[Func], Func]:
+    def inner(f: Func) -> Func:
+        _reply_functions[type] = f
+        return f
+
+    return inner
+
+
+async def reply_to(msg: Any, text: str) -> None:
+    await _reply_functions[type(msg)](msg, text)
+
+
+_answer_functions: MapToFunc = {}
+
+
+def answer_function(type: Type[Any]) -> Callable[[Func], Func]:
+    def inner(f: Func) -> Func:
+        _answer_functions[type] = f
+        return f
+
+    return inner
+
+
+async def answer_to(msg: Any, text: str) -> None:
+    await _answer_functions[type(msg)](msg, text)
